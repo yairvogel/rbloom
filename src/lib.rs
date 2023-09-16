90cebec4c8ca20013b8f00e400c30cfe4b26ca5d
Yair Vogel <yairvogel@github.com>
2023-09-16 12:23:05 +0300

    use const generic for number of hashes

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,9 @@ use std::io::Cursor;
 use bitvec::prelude::*;
 use murmur3::murmur3_32;
 
+pub struct BloomFilter<const K: usize>(pub BitArr!(for 1 << 3, in usize));
 
-trait Size {
-    const SIZE: usize;
-}
-
-pub struct BloomFilter(BitArr!(for 1 << 3, in usize));
-
-impl BloomFilter {
+impl<const K: usize> BloomFilter<K> {
     pub fn new() -> Self {
         BloomFilter(bitarr![0; 1 << 3])
     }
@@ -29,9 +24,9 @@ impl BloomFilter {
                 .all(|hash| *self.0.get(*hash as usize % self.0.len()).unwrap())
         }
 
-    fn get_hashes<T>(&self, item: &T) -> [u32; 3] where T : AsRef<[u8]> {
-        let mut hashes = [0u32; 3];
-        for seed in 0..3 {
+    fn get_hashes<T>(&self, item: &T) -> [u32; K] where T : AsRef<[u8]> {
+        let mut hashes = [0u32; K];
+        for seed in 0..K {
             hashes[seed] = murmur3_32(&mut Cursor::new(item), seed as u32).unwrap();
         }
         hashes
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use rbloom::BloomFilter;
 
 fn main() {
-    let mut bloom_filter = BloomFilter::new();
+    let mut bloom_filter = BloomFilter::<7>::new();
     
     assert_eq!(bloom_filter.test(&"hello world"), false);
     
     bloom_filter.add(&"hello world");
 
     assert_eq!(bloom_filter.test(&"hello world"), true);
+
+    let sum: usize = bloom_filter.0.iter().map(|x| *x as usize).sum();
+    assert_eq!(sum, 7);
+
     assert_eq!(bloom_filter.test(&"hello"), false);
+    bloom_filter.add(&"hello");
+    assert_eq!(bloom_filter.test(&"hello"), true);
+
     println!("success");
 }
