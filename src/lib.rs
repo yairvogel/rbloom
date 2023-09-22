b9efb88e53f2a100ff6d3ec4396251b45da9f1bf
Yair Vogel <yairvogel@github.com>
2023-09-22 12:06:32 +0300

    use fxhash

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::io::Cursor;
 use bitvec::prelude::*;
-use murmur3::murmur3_32;
+use fxhash::FxHasher;
+use core::hash::{Hash, Hasher};
 
 pub struct BloomFilter {
     bit_vec: BitVec,
@@ -9,22 +9,29 @@ pub struct BloomFilter {
 
 impl BloomFilter {
     pub fn add<T>(&mut self, item: &T)
-        where T: AsRef<[u8]> {
-        for hash in self.get_hashes(&item) {
+        where T: Hash {
+        for hash in get_hashes(item, self.k) {
             let idx = hash as usize % self.bit_vec.len();
             *self.bit_vec.get_mut(idx).unwrap() = true;
         }
     }
 
     pub fn test<T>(&self, item: &T) -> bool
-        where T: AsRef<[u8]> {
-            self.get_hashes(&item)
+        where T: Hash {
+            get_hashes(item, self.k)
                 .all(|hash| *self.bit_vec.get(hash as usize % self.bit_vec.len()).unwrap())
         }
+}
 
-    fn get_hashes<'a, T>(&self, item: &'a T) -> impl Iterator<Item = u32> + 'a where T : AsRef<[u8]> {
-        (0..self.k).map(move |seed| murmur3_32(&mut Cursor::new(item), seed as u32).unwrap())
-    }
+fn get_hashes<'a, T>(item: &'a T, k: usize) -> impl Iterator<Item = u32> + 'a where T : Hash {
+    (0..k).map(move |seed| hash_with_seed(item, seed))
+}
+
+fn hash_with_seed<T>(item: &T, seed: usize) -> u32 where T: Hash {
+    let mut hasher = FxHasher::default();
+    seed.hash(&mut hasher);
+    item.hash(&mut hasher);
+    hasher.finish() as u32
 }
 
 pub fn new(n: usize, p: f64) -> BloomFilter {
@@ -36,10 +43,9 @@ pub fn new(n: usize, p: f64) -> BloomFilter {
 }
 
 pub fn new_exact(k: usize, m:usize) -> BloomFilter {
-    let mut bit_vec = BitVec::from_element(0usize);
-    bit_vec.resize(m, false);
+    assert!(k < m, "k cannot be larger than m");
     BloomFilter {
-        bit_vec,
+        bit_vec: bitvec![0; m],
         k
     }
 }
@@ -53,6 +59,12 @@ mod tests {
         let b = new_exact(7, 100);
         assert!(b.k == 7);
         assert!(b.bit_vec.len() == 100);
+    }
+
+    #[test]
+    #[should_panic(expected = "k cannot be larger than m")]
+    fn test_panic_when_k_is_larger_than_m() {
+        new_exact(100, 10);
     }
 
     #[test]
@@ -75,5 +87,33 @@ mod tests {
         let mut b = new(100, 0.01);
         b.add(&"hello world");
         assert_eq!(b.test(&"hello"), false);
+    }
+
+    #[test]
+    fn test_with_different_types() {
+        let mut b = new(1000, 0.01);
+        assert_eq!(b.test(&false), false);
+        b.add(&false);
+        assert_eq!(b.test(&false), true);
+
+        assert_eq!(b.test(&150), false);
+        b.add(&150);
+        assert_eq!(b.test(&150), true);
+    }
+
+    #[test]
+    fn test_deterministic() {
+        let mut b1 = new_exact(3, 100);
+        let mut b2 = new_exact(3, 100);
+        b1.add(&"hello world");
+        b2.add(&"hello world");
+        assert_eq!(b1.bit_vec, b2.bit_vec);
+    }
+
+    #[test]
+    fn test_different_seeds() {
+        let mut b = new_exact(3, 100);
+        b.add(&"hello world");
+        assert_eq!(b.bit_vec.as_bitslice().count_ones(), 3);
     }
 }
